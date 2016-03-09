a0635afeb9fecdfd255615388fed25e628e60ece
Andre Christoga <andrechristoga@gmail.com>
2016-03-09 21:25:47 +0700

    Replace tipe with tingkatan

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ function submit() {
   var nama = $('#broken').val();
   var kelas = $('#arrows').val();
   var sekolah = $('#avicii').val();
-  var tipe_sekolah = $('#dj').val();
+  var tingkat_sekolah = $('#dj').val();
 
   ref.push().set({
     nama: nama,
     kelas: kelas,
     sekolah: sekolah,
-    tipe_sekolah: tipe_sekolah
+    tingkat_sekolah: tingkat_sekolah
   });
 }
