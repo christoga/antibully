b2a3993ddbf06b2affdf8ab04216747b3799097e
Andre Christoga <andrechristoga@gmail.com>
2016-03-09 21:28:35 +0700

    One variable

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 function submit() {
-  var ref = new Firebase("https://anti-bully.firebaseio.com");
-  var nama = $('#broken').val();
-  var kelas = $('#arrows').val();
-  var sekolah = $('#avicii').val();
-  var nisn = $('#better_day').val();
-  var tingkat_sekolah = $('#dj').val();
+  var ref = new Firebase("https://anti-bully.firebaseio.com"),
+      nama = $('#broken').val(),
+      kelas = $('#arrows').val(),
+      sekolah = $('#avicii').val(),
+      nisn = $('#better_day').val(),
+      tingkat_sekolah = $('#dj').val();
 
   ref.push().set({
     nama: nama,
