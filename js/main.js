2142c69dc7b3f70f5c3e844f0047f4c7a8e147bf
Andre Christoga <andrechristoga@gmail.com>
2016-03-09 21:29:52 +0700

    Clean javascript code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 function submit() {
-  var ref = new Firebase("https://anti-bully.firebaseio.com"),
-      nama = $('#broken').val(),
+  // Firebase
+  var ref = new Firebase("https://anti-bully.firebaseio.com");
+
+  // Input value
+  var nama = $('#broken').val(),
       kelas = $('#arrows').val(),
       sekolah = $('#avicii').val(),
       nisn = $('#better_day').val(),
       tingkat_sekolah = $('#dj').val();
 
+  // Send data to firebase
   ref.push().set({
     nama: nama,
     kelas: kelas,
